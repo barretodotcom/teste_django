34623be26200503eb16673a52195abd00c8fbd32
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-22 20:49:44 -0300

    teste

--- quizz_django/settings.py
+++ quizz_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l2eo=jr+&dw0x=6&(r9(dk2$u$(n$$i69mblu!#j(2d5))zs8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL =  'waste_build/static/'
+STATIC_URL =  'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
