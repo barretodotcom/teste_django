59ae7fecdff8077a29116f1f6aa1d8e4b329366e
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-27 19:48:06 -0300

    sh

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,1 @@
-echo "BUILD START" which python python3.12 -m ensurepip python3.12 -m pip install -r requirements.txt python3.12 manage.py makemigrsations python3.12 manage.py migrate python3.12 manage.py collectstatic --noinput --clear echo "BUILD END"
+echo "BUILD START" which python python3.12 -m ensurepip python3.12 -m pip install -r requirements.txt python3.12 manage.py makemigrations python3.12 manage.py migrate python3.12 manage.py collectstatic --noinput --clear echo "BUILD END"
